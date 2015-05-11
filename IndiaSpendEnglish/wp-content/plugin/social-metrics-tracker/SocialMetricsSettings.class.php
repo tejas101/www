6104892ee22aaf584821af89ca9f148629bc47ff
tejas101 <tejas@pingnetwork.in>
2015-05-11 12:30:00 +0530

    SocialMetricsTracker

---
+++ IndiaSpendEnglish/wp-content/plugin/social-metrics-tracker/SocialMetricsSettings.class.php
@@ -0,0 +1,461 @@
+<?php
+// Include and create a new WordPressSettingsFramework
+require_once( 'lib/wp-settings-framework.php' );
+
+class socialMetricsSettings {
+
+	private $wpsf;
+
+	function __construct($smt) {
+
+		$this->smt = $smt;
+
+		add_action( 'admin_menu', array(&$this, 'admin_menu'), 99 );
+
+		$section = (isset($_REQUEST['section'])) ? $_REQUEST['section'] : false;
+		switch ($section) {
+			case 'gapi':
+
+				$this->section = 'gapi';
+
+				$smt->updater->setupDataSources();
+				$this->gapi = $smt->updater->GoogleAnalyticsUpdater;
+
+				if (isset($_GET['go_to_step']) && $_GET['go_to_step']) $this->gapi->go_to_step($_GET['go_to_step']);
+
+				break;
+
+			case 'urls':
+				if (isset($_POST)) {
+					$this->process_urls_form();
+				}
+
+				$this->section = 'urls';
+				break;
+
+			case 'test':
+
+				$this->section = 'test';
+				break;
+
+			default:
+				$this->section = 'general';
+				$this->wpsf = new WordPressSettingsFramework( plugin_dir_path( __FILE__ ) .'settings/smt-'.$this->section.'.php', 'smt' );
+				break;
+		}
+
+	}
+
+	function admin_menu() {
+
+		add_submenu_page('social-metrics-tracker', 'Social Metrics Tracker Configuration', 'Configuration', 'manage_options', 'social-metrics-tracker-settings',  array($this, 'render_settings_page'));
+	}
+
+	// Display list of all and current option pages
+	function nav_links() {
+
+
+		$args = array(
+			'menu_items' => array(
+				array(
+					'slug'    => 'general',
+					'label'   => 'General Settings',
+					'url'     => 'admin.php?page=social-metrics-tracker-settings',
+					'current' => $this->section == 'general'
+				),
+				array(
+					'slug'    => 'gapi',
+					'label'   => 'Google Analytics Setup',
+					'url'     => add_query_arg('section', 'gapi'),
+					'current' => $this->section == 'gapi'
+				),
+				array(
+					'slug'    => 'urls',
+					'label'   => 'Advanced Domain / URL Setup',
+					'url'     => add_query_arg('section', 'urls'),
+					'current' => $this->section == 'urls'
+				),
+				// array(
+				// 	'slug'    => 'test',
+				// 	'label'   => 'URL Debug / Test Tool',
+				// 	'url'     => add_query_arg('section', 'test'),
+				// 	'current' => $this->section == 'test'
+				// )
+			)
+		);
+
+		print($this->smt->renderTemplate('settings-nav', $args));
+	}
+
+
+	function render_settings_page() { ?>
+		<div class="wrap">
+			<h2>Social Metrics Tracker Configuration</h2>
+			<?php $this->nav_links(); ?>
+			<?php call_user_func(array($this, $this->section.'_section')); ?>
+
+		</div>
+	<?php }
+
+
+	// Render the general settings page
+	function general_section() {
+		$this->wpsf->settings();
+	}
+
+
+	// Renders the Test Tool page
+	function test_section() {
+
+		$args = array();
+
+		$test_id = (isset($_POST['smt-test-post-id'])) ? $_POST['smt-test-post-id'] : $this->getPopularPostID();
+
+		if ($test_id > 0) {
+			$args['test-data']        = $this->fetchTestData($test_id);
+			$args['smt-test-post-id'] = $test_id;
+		}
+
+		print($this->smt->renderTemplate('settings-test', $args));
+	}
+
+
+	// Save the URL form
+	function process_urls_form() {
+		if (!isset($_POST)) return;
+
+		if (isset($_POST['url_protocol'])) $this->smt->set_smt_option('url_protocol', $_POST['url_protocol']);
+
+
+		// Domain rewrites
+		if (isset($_POST['rewrite_change_to'])) {
+			$url_rewrites = array(
+				array(
+					'rewrite_match_from'  => isset($_POST['rewrite_match_from']) ? $_POST['rewrite_match_from'] : get_home_url(),
+					'rewrite_change_to'   => $_POST['rewrite_change_to'],
+					'rewrite_before_date' => $_POST['rewrite_before_date']
+				)
+			);
+
+			$this->smt->set_smt_option('url_rewrites', $url_rewrites);
+		} else {
+			$this->smt->delete_smt_option('url_rewrites');
+		}
+
+		// Performacne
+		if (isset($_POST['alt_url_ttl_multiplier'])) {
+			$this->smt->set_smt_option('alt_url_ttl_multiplier', $_POST['alt_url_ttl_multiplier']);
+		}
+
+	}
+
+	// Render the URLs settings page
+	function urls_section() {
+
+		$args = array();
+
+		// URL Protocol
+		$url_protocol = $this->smt->get_smt_option('url_protocol');
+
+		$args['protocol_options'] = array(
+			array(
+				'key'   => 'auto',
+				'label' => 'Auto (let WordPress decide)',
+				'selected' => ($url_protocol == 'auto')
+			),
+			array(
+				'key'   => 'http',
+				'label' => 'Check only http:// versions of URLs',
+				'selected' => ($url_protocol == 'http')
+			),
+			array(
+				'key'   => 'https',
+				'label' => 'Check only https:// versions of URLs',
+				'selected' => ($url_protocol == 'https')
+			),
+			array(
+				'key'   => 'both',
+				'label' => 'Check both http:// and https:// URLs',
+				'selected' => ($url_protocol == 'both')
+			),
+		);
+
+
+		// Domain migration
+		$url_rewrites = $this->smt->get_smt_option('url_rewrites');
+
+		$args['rewrite_match_from']  = $url_rewrites[0]['rewrite_match_from'] ? $url_rewrites[0]['rewrite_match_from'] : get_home_url();
+		$args['rewrite_change_to']   = $url_rewrites[0]['rewrite_change_to'] ? $url_rewrites[0]['rewrite_change_to'] : '';
+		$args['rewrite_before_date'] = $url_rewrites[0]['rewrite_before_date'] ? $url_rewrites[0]['rewrite_before_date'] : '';
+
+		$args['example_match_from'] = get_permalink($this->getPopularPostID());
+
+
+		// Performance Option
+		$current_alt_ttl_option = ($this->smt->get_smt_option('alt_url_ttl_multiplier')) ? $this->smt->get_smt_option('alt_url_ttl_multiplier') : 5;
+
+		$args['alt_url_ttl_multiplier_options'] = array(
+			array(
+				'key'   => '1',
+				'label' => 'Same as main URL refresh rate (uses more network requests)',
+				'selected' => ($current_alt_ttl_option == '1')
+			),
+			array(
+				'key'   => '5',
+				'label' => 'Slightly slower refresh rate',
+				'selected' => ($current_alt_ttl_option == '5')
+			),
+			array(
+				'key'   => '10',
+				'label' => 'Much slower refresh rate (better server performance)',
+				'selected' => ($current_alt_ttl_option == '10')
+			)
+		);
+
+
+		print($this->smt->renderTemplate('settings-urls', $args));
+	}
+
+	// Returns some live data from social networks for a variety of URL filters
+	function fetchTestData($post_id) {
+		$permalink = $this->smt->updater->adjustProtocol(get_permalink($post_id));
+		return $this->smt->updater->fetchPostStats($post_id, $permalink);
+	}
+
+	// Returns a post ID for the most shared post
+	function getPopularPostID() {
+
+		$args = array(
+			'posts_per_page'   => 1,
+			'offset'           => 0,
+			'orderby'          => 'meta_value',
+			'order'            => 'DESC',
+			'meta_key'         => 'social_aggregate_score',
+			'post_status'      => 'publish',
+			'suppress_filters' => true 
+		);
+		$posts_array = get_posts( $args );
+
+		return $posts_array[0]->ID;
+
+	}
+
+	// Render the Google API config page
+	//
+	// Beware: Messy PHP code within.....
+	function gapi_section() {
+
+		if (!$this->gapi) {
+			print('<h1>Google Analytics Integration</h1><h3 style="color:red;">Sorry, Google Analytics integration can\'t be configured because this appears to be a development server. </h3>');
+			return;
+		}
+
+		?>
+
+		<h1>Google Analytics Integration</h1>
+		<p>This plugin can sync and display page view data from Google Analytics. </p>
+		<hr>
+
+		<?php
+
+		// Display progress navigator
+		$this->gapi_wizard_progress();
+
+		// Display a different HTML block per each wizard step
+		switch ($this->gapi->step) {
+			/*************************************************/
+			case 1:
+		  	/*************************************************/
+			?>
+			<form method="POST" action="admin.php?page=social-metrics-tracker-settings&section=gapi">
+
+			<?php if (is_multisite() && current_user_can( 'manage_network' )) : ?>
+				<h2>Multisite Configuration</h2>
+
+				<table class="form-table">
+				<tbody>
+					<tr class="blue-box">
+						<th scope="row">WP Multisite Mode</th>
+						<td>
+							<select name="multisite_mode">
+							  <option value="0"<?php if (!$this->gapi->multisite_mode) echo ' selected'; ?>>Different credentials per blog (default)</option>
+							  <option value="1"<?php if ($this->gapi->multisite_mode) echo ' selected'; ?>>Shared credentials for all blogs</option>
+							</select>
+							<p class="description">With <b>shared credentials</b>, all blogs on this WP Multisite network will automatically use the Google Analytics profile you set up to sync data and you will only need to complete this setup wizard once. With <b>different credentials</b>, each blog will need to complete this wizard but each blog will be able to link to a seperate Gooogle Analytics account.</p>
+						</td>
+					</tr>
+				</tbody>
+				</table>
+
+			<?php endif; ?>
+
+			<h2>Google Developer account info</h2>
+			<p>Setting up a Google Developer account and entering your details here gives this plugin the ability to interact with the API for Google Services. This is different than your Google Analytics account, which will link to on the next page.</p>
+
+			<ol>
+				<li><a href="https://console.developers.google.com" target="_new">Set up a free Google Developer account</a></li>
+				<li>Create a new project</li>
+				<li>Enable the Google Analytics API for the project</li>
+				<li>Under <b>APIs & auth > Credentials</b> click "Create new Client ID" and select "Web Appplication"</li>
+				<li>Add the following authorized redirect URI: <input value="<?php echo $this->gapi->redirect_uri; ?>" onClick="this.select();" style="min-width:300px; background:white; cursor:text; font-size:11px" readonly></li>
+				<li>Enter the information you receive below: </li>
+			</ol>
+
+			<?php $keys = $this->gapi->get_gapi_keys(); ?>
+			<table class="form-table yellow-box">
+			<tbody>
+				<tr>
+					<th scope="row">Client ID</th>
+					<td><input type="text" name="gapi_client_id" id="gapi_client_id" value="<?php echo $keys['gapi_client_id']; ?>" class="regular-text" placeholder="example: 123.apps.googleusercontent.com"></td>
+				</tr>
+				<tr>
+					<th scope="row">Email Address (API Key)</th>
+					<td><input type="text" name="gapi_developer_key" id="gapi_developer_key" value="<?php echo $keys['gapi_developer_key']; ?>" class="regular-text" placeholder="example: 123@developer.gserviceaccount.com"></td>
+				</tr>
+				<tr>
+					<th scope="row">Client Secret</th>
+					<td><input type="text" name="gapi_client_secret" id="gapi_client_secret" value="<?php echo $keys['gapi_client_secret']; ?>" class="regular-text" placeholder="example: 123abc"></td>
+				</tr>
+
+			</tbody>
+			</table>
+
+			<input type="submit" name="submit" id="submit" class="button button-primary button-large" value="Continue to next step">
+
+			</form>
+
+			<?php
+			/*************************************************/
+			break; case 2:
+		  	/*************************************************/
+			?>
+
+			<h2>Google Analytics sign-in</h2>
+			<p>Sign in to your Google Analytics account and grant access to share read-only data with this plugin. The button below will take you to Google to sign-in. This plugin will save your OAuth permissions locally on this blog until you revoke them. </p>
+
+			<a class="button-primary" id="gapi-sign-in" href="<?php echo $this->gapi->get_oauth_login_url(); ?>">Sign in with Google Analytics</a>
+
+			<br />
+
+			<div class="yellow-box">
+				<p>If you see "Error: redirect_uri_mismatch" then you probably still need to add the authorization URI below to your <a href="https://console.developers.google.com" target="_new">Google Developer account</a>.</p>
+
+				<label><b>Redirect URI Authorization</b></label><br />
+				<small>You must enter this when you create your Google Developer web application</small><br />
+				<input value="<?php echo $this->gapi->redirect_uri; ?>" onClick="this.select();" style="width:600px; background:white; cursor:text; font-size:11px" readonly><br />
+				<br />
+			</div>
+
+			<?php
+			/*************************************************/
+			break; case 3:
+		  	/*************************************************/
+			?>
+
+			<h2>Select a reporting profile</h2>
+
+			<form method="POST" action="admin.php?page=social-metrics-tracker-settings&section=gapi">
+
+			<table class="form-table">
+			<tbody>
+				<tr>
+					<th scope="row">Analytics Profile</th>
+					<td>
+						<select name="gapi_profile_id">
+						<?php $profiles = $this->gapi->get_profile_list(); ?>
+						<?php foreach($profiles as $profile) : ?>
+							<option value="<?php echo $profile['id'].',['.$profile['parent'].'] '. $profile['name']; ?>">
+								<?php echo '['.$profile['parent'].'] '. $profile['name']; ?>
+							</option>
+						<?php endforeach; ?>
+						</select>
+
+						<?php if (is_multisite() && current_user_can( 'manage_network' )) : ?>
+
+						<p class="description">With <b>shared credentials</b>, all blogs on this WP Multisite network will automatically use the Google Analytics profile you set up to sync data and you will only need to complete this setup wizard once. With <b>different credentials</b>, each blog will need to complete this wizard but each blog will be able to link to a seperate Gooogle Analytics account.</p>
+
+						<?php endif; ?>
+
+					</td>
+				</tr>
+			</tbody>
+			</table>
+
+			<input type="submit" name="submit" id="submit" class="button button-primary button-large" value="Continue to next step">
+
+			</form>
+
+			<?php
+			/*************************************************/
+			break; case 4:
+		  	/*************************************************/
+			?>
+
+			<h2>Setup complete. Connected to: </h2>
+			<h3><?php echo $this->gapi->get_profile_name(); ?></h3>
+			<p>Data will sync in the background. It will take up to one day for all data to be imported the first time around. </p>
+
+			<?php if ($this->gapi->multisite_mode) : ?>
+				<h3>Multisite data sharing is enabled</h3>
+				<p>All blogs will sync with this Google Analytics account.</p>
+			<?php endif; ?>
+
+			<p>If you wish to disable data sync, navigate back to any of the previous setup steps and data syncing will stop.</p>
+
+			<?php
+			/*************************************************/
+			break; default:
+		  	/*************************************************/
+			?>
+			<h2>Something isn't working right...</h2>
+			<?php
+			/*************************************************/
+			break;
+		  	/*************************************************/
+
+		} // end switch
+
+
+	} // end gapi_section();
+
+	// Displays a <ol> nav bar with the current step highlighted
+	function gapi_wizard_progress() {
+
+		$this->gapi->connect();
+
+		$steps = array(
+			1 => '1. Set up Google API developer keys',
+			2 => '2. Login with OAuth',
+			3 => '3. Select reporting profile',
+			4 => 'Sync with Google Analytics'
+		);
+
+		if ($this->gapi->is_ready) {
+			print('<ol id="smt_gapi_steps" class="ready">');
+		} else {
+			print('<ol id="smt_gapi_steps">');
+		}
+
+		foreach ($steps as $num => $step) {
+
+			// Step already completed
+			if ($this->gapi->step > $num) {
+				print('<li class="done"><a href="admin.php?page=social-metrics-tracker-settings&section=gapi&go_to_step='.$num.'" class="btn" onclick="return confirm(\'If you go back to this step, data will not sync until all steps are complete.\')">'.$step.'</a></li>');
+			}
+
+			// Step in progress
+			if ($this->gapi->step == $num) {
+				print('<li class="current"><div class="btn">'.$step.'</div></li>');
+			}
+
+			// Steps coming up next
+			if ($this->gapi->step < $num) {
+				print('<li class="future"><div class="btn">'.$step.'</div></li>');
+			}
+
+		}
+
+		print('</ol>');
+	}
+
+}
---
+++ IndiaSpendEnglish/wp-content/plugin/social-metrics-tracker/social-metrics-tracker.php
@@ -0,0 +1,333 @@
+<?php
+/*
+Plugin Name: Social Metrics Tracker
+Plugin URI: https://github.com/ChapmanU/wp-social-metrics-tracker
+Description: Collect and display social network shares, likes, tweets, and view counts of posts.
+Version: 1.4.0
+Author: Ben Cole, Chapman University
+Author URI: http://www.bencole.net
+License: GPLv2+
+*/
+
+/**
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License, version 2 or, at
+ * your discretion, any later version, as published by the Free
+ * Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ */
+
+
+// Class Dependancies
+require_once('MetricsUpdater.class.php');
+require_once('data-sources/google_analytics.php');
+include_once('SocialMetricsSettings.class.php');
+include_once('SocialMetricsTrackerWidget.class.php');
+include_once('SocialMetricsDebugger.class.php');
+
+// Handlebars Autoloader
+require_once('lib/Handlebars/Autoloader.php');
+Handlebars_Autoloader::register();
+// use Handlebars\Handlebars;
+
+
+class SocialMetricsTracker {
+
+	public $version = '1.4.0'; // for db upgrade comparison
+	public $updater;
+	public $options;
+
+	public function __construct() {
+
+		// Plugin activation hooks
+		register_activation_hook( __FILE__, array($this, 'activate') );
+		register_deactivation_hook( __FILE__, array($this, 'deactivate') );
+
+		if (is_admin()) {
+			add_action('admin_menu', array($this,'adminMenuSetup'));
+			add_action('admin_enqueue_scripts', array($this, 'adminHeaderScripts'));
+			add_action('plugins_loaded', array($this, 'version_check'));
+			add_action('wp_dashboard_setup', array($this, 'dashboard_setup'));
+		}
+
+		add_action('init', array($this, 'init'));
+
+	} // end constructor
+
+	public function init() {
+
+		// Set up options
+		$this->options = get_option('smt_settings', array());
+
+		// Ensure setup occurs for each blog when network activated
+		if (empty($this->options)) $this->activate();
+
+		// Development server notice
+		if ($this->is_development_server()) {
+			add_action('admin_notices', array($this, 'developmentServerNotice'));
+		}
+
+		$this->updater  = new MetricsUpdater($this);
+		$this->debugger = new SocialMetricsDebugger($this);
+
+		// Data export tool
+		if (is_admin() && isset($_GET['smt_download_export_file']) && $_GET['smt_download_export_file'] && $_GET['page'] == 'social-metrics-tracker-export') {
+			require('smt-export.php');
+			smt_download_export_file($this);
+		}
+	}
+
+	/***************************************************
+	* Renders a template using the Handlebars Engine
+	***************************************************/
+	public function renderTemplate($tpl, $data) {
+
+		if (!isset($this->template_engine)) {
+
+			$this->template_engine = new Handlebars_Engine(array(
+			    'loader' => new Handlebars_Loader_FilesystemLoader(dirname(__FILE__).'/templates/'),
+			    'partials_loader' => new Handlebars_Loader_FilesystemLoader(
+			        dirname(__FILE__).'/templates/',
+			        array(
+			            'prefix' => '_'
+			        )
+			    )
+			));
+
+		}
+
+		return $this->template_engine->render($tpl, $data);
+	}
+
+	// Determines if we are on a development or staging environment
+	public function is_development_server() {
+		return (defined('WP_ENV') && strtolower(WP_ENV) != 'production');
+	}
+
+	public function developmentServerNotice() {
+		if (!current_user_can('manage_options')) return false;
+
+		$screen = get_current_screen();
+
+		if (!in_array($screen->base, array('social-metrics_page_social-metrics-tracker-settings', 'toplevel_page_social-metrics-tracker', 'social-metrics-tracker_page_social-metrics-tracker-debug'))) {
+			return false;
+		}
+
+		$message = '<h3 style="margin-top:0;">Social Metrics data syncing is disabled</h3> You are on a development server; Social Network share data cannot be retrieved for private development URLs. ';
+
+		$message .= "<ul><li>The PHP constant <b>WP_ENV</b> must be set to <b>production</b> or be undefined. WP_ENV is currently set to: <b>".WP_ENV."</b>. </li></ul>";
+
+		printf( '<div class="error"> <p> %s </p> </div>', $message);
+
+	}
+
+	public function adminHeaderScripts() {
+
+		wp_register_style( 'smt-css', plugins_url( 'css/social-metrics-tracker.min.css' , __FILE__ ), false, $this->version );
+		wp_enqueue_style( 'smt-css' );
+
+		wp_register_script( 'smt-js', plugins_url( 'js/social-metrics-tracker.min.js' , __FILE__ ), array('jquery', 'jquery-ui-datepicker'), $this->version );
+		wp_enqueue_script( 'smt-js' );
+
+		wp_enqueue_style('jquery-style', 'http://ajax.googleapis.com/ajax/libs/jqueryui/1.8.2/themes/smoothness/jquery-ui.css');
+
+	} // end adminHeaderScripts()
+
+	public function adminMenuSetup() {
+
+		// Add Social Metrics Tracker menu
+		$visibility = ($this->options['smt_options_report_visibility']) ? $this->options['smt_options_report_visibility'] : 'manage_options';
+		add_menu_page( 'Social Metrics Tracker', 'Social Metrics', $visibility, 'social-metrics-tracker', array($this, 'render_view_Dashboard'), 'dashicons-chart-area', '30.597831' );
+
+		// Add advanced stats menu
+		if ($this->options['smt_options_debug_mode']) {
+			$debug_visibility = ($this->options['smt_options_debug_report_visibility']) ? $this->options['smt_options_debug_report_visibility'] : 'manage_options';
+			add_submenu_page('social-metrics-tracker', 'Relevancy Rank', 'Debug Info', $debug_visibility, 'social-metrics-tracker-debug',  array($this, 'render_view_AdvancedDashboard'));
+		}
+
+		// Export page
+		add_submenu_page('social-metrics-tracker', 'Data Export Tool', 'Export Data', $visibility, 'social-metrics-tracker-export',  array($this, 'render_view_export'));
+
+		new socialMetricsSettings($this);
+
+	} // end adminMenuSetup()
+
+	public function dashboard_setup() {
+		new SocialMetricsTrackerWidget($this);
+	}
+
+	public function render_view_Dashboard() {
+		require('smt-dashboard.php');
+		smt_render_dashboard_view($this);
+	} // end render_view_Dashboard()
+
+	public function render_view_AdvancedDashboard() {
+		require('smt-dashboard-debug.php');
+		smt_render_dashboard_debug_view($this->options);
+	} // end render_view_AdvancedDashboard()
+
+	public function render_view_export() {
+		require('smt-export.php');
+		smt_render_export_view($this);
+	}
+
+	public function render_view_Settings() {
+		require('smc-settings-view.php');
+		smc_render_settings_view();
+	} // end render_view_Settings()
+
+	public static function timeago($time) {
+		$periods = array("second", "minute", "hour", "day", "week", "month", "year", "decade");
+		$lengths = array("60","60","24","7","4.35","12","10");
+
+		$now = current_time( 'timestamp' );
+
+			$difference     = $now - $time;
+			$tense         = "ago";
+
+		for($j = 0; $difference >= $lengths[$j] && $j < count($lengths)-1; $j++) {
+			$difference /= $lengths[$j];
+		}
+
+		$difference = round($difference);
+
+		if($difference != 1) {
+			$periods[$j].= "s";
+		}
+
+		return "$difference $periods[$j] ago";
+	}
+
+	/***************************************************
+	* Return an array of the post types we are currently tracking
+	***************************************************/
+	public function tracked_post_types() {
+		$types_to_track = array();
+
+		$smt_post_types = get_post_types( array( 'public' => true ), 'names' );
+		unset($smt_post_types['attachment']);
+
+		foreach ($smt_post_types as $type) {
+			if (isset($this->options['smt_options_post_types_'.$type]) && $this->options['smt_options_post_types_'.$type] == $type) $types_to_track[] = $type;
+		}
+
+		// If none selected, default post types
+		return ($types_to_track) ? $types_to_track : array_values($smt_post_types);
+	}
+
+	/***************************************************
+	* Check the version of the plugin and perform upgrade tasks if necessary
+	***************************************************/
+	public function version_check() {
+		$installed_version = get_option( "smt_version" );
+
+		if( $installed_version != $this->version ) {
+
+			// **********************
+			// Perform upgrade tasks:
+
+			// 1: Update version number
+			update_option( "smt_version", $this->version );
+
+			// 2: If migrating from version below 1.3 (not a clean install)
+			if ($installed_version !== false && version_compare($installed_version, '1.3', '<')) {
+
+				// Do not require an initial full data sync for migrating users. 
+				update_option( 'smt_last_full_sync', 1 );
+			}
+
+			// 3: If migrating from version below 1.4.0 (not a clean install)
+			if ($installed_version !== false && version_compare($installed_version, '1.4.0', '<')) {
+
+				// Prior to 1.4.0, the system defaulted to http:// URLs
+				$this->set_smt_option('url_protocol', 'http');
+
+				// The debug option is no longer in use
+				$this->delete_smt_option('debug_mode');
+			}
+
+			// 4: Add any new settings
+			$this->add_missing_settings();
+
+		}
+	}
+
+	/***************************************************
+	* Runs at plugin activation;
+	* Also runs at initialization in event that option defaults have not been set for some reason
+	***************************************************/
+	public function activate() {
+
+		// Set default post types to track
+		$this->set_smt_option('post_types_post', 'post', false);
+		$this->set_smt_option('post_types_page', 'page', false);
+		$this->add_missing_settings(); // Also saves the two above
+
+		$this->version_check();
+	}
+
+	/***************************************************
+	* Checks all of the settings and if any are undefined, adds them from the defaults
+	***************************************************/
+	public function add_missing_settings() {
+		require('settings/smt-general.php');
+		global $wpsf_settings;
+
+		foreach ($wpsf_settings[0]['fields'] as $default) {
+			$key = $default['id'];
+
+			if ($this->get_smt_option($key) === false) {
+				$this->set_smt_option($key, $default['std'], false);
+			}
+		}
+
+		$this->save_smt_options();
+	}
+
+	/***************************************************
+	* Get plugin option with the specified key
+	***************************************************/
+	public function get_smt_option($key) {
+		return (array_key_exists('smt_options_'.$key, $this->options)) ? $this->options['smt_options_'.$key] : false;
+	}
+
+	/***************************************************
+	* Update and optionally save plugin option with the specified key/value
+	* (We might not want to save if we are bulk updating)
+	***************************************************/
+	public function set_smt_option($key, $val, $save = true) {
+		$this->options['smt_options_'.$key] = $val;
+		return ($save) ? $this->save_smt_options() : null;
+	}
+
+	/***************************************************
+	* Remove specified option
+	***************************************************/
+	public function delete_smt_option($key) {
+		unset($this->options['smt_options_'.$key]);
+		return $this->save_smt_options();
+	}
+
+	/***************************************************
+	* Saves the settings to the DB
+	***************************************************/
+	private function save_smt_options() {
+		return update_option('smt_settings', $this->options);
+	}
+
+	public function deactivate() {
+
+		// Remove Queued Updates
+		MetricsUpdater::removeAllQueuedUpdates();
+
+	}
+
+} // END SocialMetricsTracker
+
+// Run plugin
+$SocialMetricsTracker = new SocialMetricsTracker();
